25ef296aab3eb48be7703c123765879168151bec
Daniil Shevtsov <shevtsov200@gmail.com>
2018-04-22 15:23:32 +0300

    Add functionality for registry key saves.

--- ReplaceSaves.bat
+++ ReplaceSaves.bat
@@ -9,8 +9,8 @@ REM location of profiles
 set PROFILES_LOCATION=profiles
 
 REM save type codes
-set DIRECTORY_SAVE_CHAR=d
-set REGISTRY_SAVE_CHAR=r
+set DIRECTORY_SAVE_CHAR=D
+set REGISTRY_SAVE_CHAR=R
 
 if not exist %GAMES_FILE_NAME% type nul>%GAMES_FILE_NAME%
 if not exist %PROFILES_FILE_NAME% type nul>%PROFILES_FILE_NAME%
@@ -49,12 +49,21 @@ if "%ERRORLEVEL%" == "1" (
 		
 		REM add games to this profile directory
 		REM parse games file for game names and saves locations
-		for /F "tokens=1,2,3 delims=# " %%A in (%GAMES_FILE_NAME%) do (
-			echo name: "%%A" location: "%%B" type: "%%C"
+		for /F "tokens=1,2,3 delims=#" %%A in (%GAMES_FILE_NAME%) do (
 			REM create game directory
 			mkdir "%PROFILES_LOCATION%\!profileName!\%%A"
-			REM copy game saves to this profile directory
-			robocopy "%%B" "%PROFILES_LOCATION%\!profileName!\%%A" /e /njh /njs /ndl /nc /ns
+			
+			if %%C == %DIRECTORY_SAVE_CHAR% (			
+				REM copy game saves to this profile directory
+				robocopy "%%B" "%PROFILES_LOCATION%\!profileName!\%%A" /e /njh /njs /ndl /nc /ns
+			) else if %%C == %REGISTRY_SAVE_CHAR% (
+				REM export registry to this profile directory
+				REG EXPORT "%B%" "%PROFILES_LOCATION%\!profileName!\%%A\%%A.reg"
+			) else (
+				echo unrecognized save type: %%C
+			)
+			echo name: "%%A" location: "%%B" type: "%%C"
+			
 		)
 	)
 REM choose another profile
@@ -69,10 +78,21 @@ REM choose another profile
 		REM parse games file for game names and save locations
 		for /F "tokens=1,2,3 delims=#" %%A in (%GAMES_FILE_NAME%) do (
 			echo name: "%%A" location: "%%B" type: "%%C"
-			REM copy current profile saves to its directory
-			robocopy "%%j" "%PROFILES_LOCATION%\!currentProfile!\%%A" /e /njh /njs /ndl /nc /ns
-			REM replace current saves with saves of new profile
-			robocopy "%PROFILES_LOCATION%\!profileName!\%%A" "%%B" /e /njh /njs /ndl /nc /ns
+			if %%C == %DIRECTORY_SAVE_CHAR% (
+				REM copy current profile saves to its directory
+				robocopy "%%B" "%PROFILES_LOCATION%\!currentProfile!\%%A" /e /njh /njs /ndl /nc /ns
+				REM replace current saves with saves of new profile
+				robocopy "%PROFILES_LOCATION%\!profileName!\%%A" "%%B" /e /njh /njs /ndl /nc /ns
+			) else if %%C == %REGISTRY_SAVE_CHAR% (
+				REM export current profile save registry to its directory
+				echo "%%B" "%PROFILES_LOCATION%\!currentProfile!\%%A\%%A.reg"
+				REG EXPORT "%%B" "%PROFILES_LOCATION%\!currentProfile!\%%A\%%A.reg" /y
+				REM replace current save registry with saves of new profile
+				REG IMPORT "%PROFILES_LOCATION%\!profileName!\%%A\%%A.reg"
+			) else (
+				echo unrecognized save type: %%C
+			)
+
 		)
 		REM save new current profile name
 		echo !profileName!>%CURRENT_PROFILE_FILE_NAME%
@@ -82,7 +102,7 @@ REM choose another profile
 REM add new game
 ) else if "%ERRORLEVEL%" == "3" (
 	REM read save type ^(directory or registry key^)
-	echo write save type ^(^'d^' for directory or ^'r^' for registry key^)
+	echo write save type ^(^'%DIRECTORY_SAVE_CHAR%^' for directory or ^'%REGISTRY_SAVE_CHAR%^' for registry key^)
 	set /p saveType=
 	echo "!saveType!"
 	
@@ -103,7 +123,7 @@ REM add new game
 		
 		REM append game name and save location to games file
 		(
-			echo ^#!gameName!^#!savesLocation!
+			echo ^#!gameName!^#!savesLocation!^#!saveType!
 		) >> %GAMES_FILE_NAME%
 		
 		REM copy saves of this game to every profile
@@ -114,8 +134,31 @@ REM add new game
 		) 
 	) else if "!saveType!" == "%REGISTRY_SAVE_CHAR%" (
 		REM saves in registry key
-		echo registry save
-		REG E
+		echo registry key save
+		
+		REM read new game name
+		echo write game name
+		set /p gameName=
+		
+		echo "!gameName!"
+		REM read location of saves
+		echo write saves registry key
+		set /p registryKey=
+		echo !registryKey!
+		
+		REM append game name and save location to games file
+		(
+			echo ^#!gameName!^#!registryKey!^#!saveType!
+		) >> %GAMES_FILE_NAME%
+		
+		REM copy saves of this game to every profile
+		for /F "tokens=*" %%A in (%PROFILES_FILE_NAME%) do (
+			echo "%%A"
+			mkdir "%PROFILES_LOCATION%\%%A\!gameName!"
+			REM export game registry branch
+			echo "!registryKey!" "%cd%\%PROFILES_LOCATION%\%%A\!gameName!"
+			REG EXPORT "!registryKey!" "%cd%\%PROFILES_LOCATION%\%%A\!gameName!\!gameName!.reg"
+		) 
 	) else (
 		REM incorrect save type char
 		echo save type "!saveType!" is not recognized
@@ -124,6 +167,7 @@ REM add new game
 	goto exit
 )
 goto menu
+
 :exit
 PAUSE
 
